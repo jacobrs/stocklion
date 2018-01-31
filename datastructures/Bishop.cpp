7d9552760cea2e3ca52fdf596a084d9109be721e
Jacob Gagne <jacob_gagne@hotmail.com>
2018-01-31 13:57:02 -0500

    implemented bishop logic

--- datastructures/Bishop.cpp
+++ datastructures/Bishop.cpp
@@ -3,5 +3,38 @@
 Bishop::Bishop(const Position &initialPosition, const Color &initialPlayer) : Piece(initialPosition, initialPlayer) {}
 
 std::vector<Position> Bishop::possibleMoves() {
-    return std::vector<Position>();
+
+    std::vector<Position> positions;
+
+    // top left to bottom right
+    Position comparison = currentPosition;
+    while(comparison.row < 8 && comparison.column > 'A'){
+        comparison.row ++;
+        comparison.column --;
+    }
+
+    while(comparison.row >= 1 && comparison.column <= 'H'){
+        if(comparison.row != currentPosition.row && comparison.column != currentPosition.column) {
+            positions.push_back(comparison);
+        }
+        comparison.row--;
+        comparison.column++;
+    }
+
+    // bottom left to top right
+    comparison = currentPosition;
+    while(comparison.row > 1 && comparison.column > 'A'){
+        comparison.row --;
+        comparison.column --;
+    }
+
+    while(comparison.row <= 8 && comparison.column <= 'H'){
+        if(comparison.row != currentPosition.row && comparison.column != currentPosition.column) {
+            positions.push_back(comparison);
+        }
+        comparison.row ++;
+        comparison.column ++;
+    }
+
+    return positions;
 }
--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "datastructures/Pawn.h"
+#include "datastructures/Bishop.h"
 
 
 int main() {
 
     Position initialPosition = { 'E', 2 };
     Pawn *p = new Pawn(initialPosition, WHITE);
+    Bishop *bishop = new Bishop({ 'E', 4}, WHITE);
 
     for(auto position: p->possibleMoves()){
+        std::cout << position.column << position.row << " ";
+    }
+
+    std::cout << std::endl;
+
+    for(auto position: bishop->possibleMoves()){
         std::cout << position.column << position.row << " ";
     }
 
