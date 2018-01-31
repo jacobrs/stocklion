4e87caef108844b5f412632337856c7495233871
Jacob Gagne <jacob_gagne@hotmail.com>
2018-01-31 14:06:06 -0500

    implemented rook move logic

--- datastructures/Rook.cpp
+++ datastructures/Rook.cpp
@@ -3,5 +3,19 @@
 Rook::Rook(const Position &initialPosition, const Color &initialPlayer) : Piece(initialPosition, initialPlayer) {}
 
 std::vector<Position> Rook::possibleMoves() {
-    return std::vector<Position>();
+    std::vector<Position> positions;
+
+    for(char i = 'A'; i <= 'H'; i++){
+        if(i != currentPosition.column){
+            positions.push_back({ i, currentPosition.row });
+        }
+    }
+
+    for(int i = 1; i <= 8; i++){
+        if(i != currentPosition.row){
+            positions.push_back( { currentPosition.column, i });
+        }
+    }
+
+    return positions;
 }
--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "datastructures/Pawn.h"
 #include "datastructures/Bishop.h"
+#include "datastructures/Rook.h"
 
 
 int main() {
 
     Position initialPosition = { 'E', 2 };
     Pawn *p = new Pawn(initialPosition, WHITE);
-    Bishop *bishop = new Bishop({ 'E', 4}, WHITE);
+    Bishop *bishop = new Bishop({ 'E', 4 }, WHITE);
+    Rook *rook = new Rook({ 'E', 4 }, BLACK);
 
     for(auto position: p->possibleMoves()){
         std::cout << position.column << position.row << " ";
@@ -16,6 +18,12 @@ int main() {
     std::cout << std::endl;
 
     for(auto position: bishop->possibleMoves()){
+        std::cout << position.column << position.row << " ";
+    }
+
+    std::cout << std::endl;
+
+    for(auto position: rook->possibleMoves()){
         std::cout << position.column << position.row << " ";
     }
 
