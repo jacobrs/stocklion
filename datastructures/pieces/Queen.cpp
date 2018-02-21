1413508e28bd751b863a502f49b3493e6f6b41bc
Jacob Gagne <jacob_gagne@hotmail.com>
2018-02-21 00:19:02 -0500

    refactored diagonal logic and added unit tests

--- datastructures/pieces/Queen.cpp
+++ datastructures/pieces/Queen.cpp
@@ -6,68 +6,76 @@ Queen::Queen(const Position &initialPosition, const Color &initialPlayer) : Piec
 std::vector<Position> Queen::possibleDirectMoves(Board &board) {
     std::vector<Position> positions;
 
-    // top left to bottom right
+    // to top left
     Position comparison = currentPosition;
     Piece *potentialPiece;
-    while(comparison.row < 8 && comparison.column > 'A'){
-        comparison.row ++;
-        comparison.column --;
+    comparison.row++;
+    comparison.column--;
+    while(comparison.row <= 8 && comparison.column >= 'A') {
         potentialPiece = board.getPiece(comparison);
-        if(potentialPiece != nullptr && potentialPiece != this){
+        if(potentialPiece != nullptr){
+            if(potentialPiece->player != player){
+                positions.push_back(comparison);
+            }
             break;
+        }else{
+            positions.push_back(comparison);
+            comparison.row++;
+            comparison.column--;
         }
     }
 
-    while(comparison.row >= 1 && comparison.column <= 'H'){
+    // to bottom right
+    comparison = currentPosition;
+    comparison.row--;
+    comparison.column++;
+    while(comparison.row >= 1 && comparison.column <= 'H') {
         potentialPiece = board.getPiece(comparison);
-        if(potentialPiece != nullptr && potentialPiece->player == player && potentialPiece != this){
+        if(potentialPiece != nullptr){
+            if(potentialPiece->player != player){
+                positions.push_back(comparison);
+            }
+            break;
+        }else{
+            positions.push_back(comparison);
             comparison.row--;
             comparison.column++;
-            continue;
-        }
-        if(comparison.row != currentPosition.row && comparison.column != currentPosition.column) {
-            positions.push_back(comparison);
-        }
-        comparison.row--;
-        comparison.column++;
-        potentialPiece = board.getPiece(comparison);
-        if(potentialPiece != nullptr && potentialPiece != this){
-            if(potentialPiece->player != player){
-                positions.push_back(comparison);
-            }
-            break;
         }
     }
 
-    // bottom left to top right
+    // to bottom left
     comparison = currentPosition;
-    while(comparison.row > 1 && comparison.column > 'A'){
-        comparison.row --;
-        comparison.column --;
+    comparison.row--;
+    comparison.column--;
+    while(comparison.row >= 1 && comparison.column >= 'A') {
         potentialPiece = board.getPiece(comparison);
-        if(potentialPiece != nullptr && potentialPiece != this){
-            break;
-        }
-    }
-
-    while(comparison.row <= 8 && comparison.column <= 'H'){
-        potentialPiece = board.getPiece(comparison);
-        if(potentialPiece != nullptr && potentialPiece->player == player && potentialPiece != this){
-            comparison.row ++;
-            comparison.column ++;
-            continue;
-        }
-        if(comparison.row != currentPosition.row && comparison.column != currentPosition.column) {
-            positions.push_back(comparison);
-        }
-        comparison.row ++;
-        comparison.column ++;
-        potentialPiece = board.getPiece(comparison);
-        if(potentialPiece != nullptr && potentialPiece != this){
+        if(potentialPiece != nullptr){
             if(potentialPiece->player != player){
                 positions.push_back(comparison);
             }
             break;
+        }else{
+            positions.push_back(comparison);
+            comparison.row--;
+            comparison.column--;
+        }
+    }
+
+    // to top right
+    comparison = currentPosition;
+    comparison.row++;
+    comparison.column++;
+    while(comparison.row <= 8 && comparison.column <= 'H') {
+        potentialPiece = board.getPiece(comparison);
+        if(potentialPiece != nullptr){
+            if(potentialPiece->player != player){
+                positions.push_back(comparison);
+            }
+            break;
+        }else{
+            positions.push_back(comparison);
+            comparison.row++;
+            comparison.column++;
         }
     }
 
